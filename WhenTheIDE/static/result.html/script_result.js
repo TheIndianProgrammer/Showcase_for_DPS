acf76e8f58910502317b0287296f2886feb71d27
TheIndianProgrammer <64490665+TheIndianProgrammer@users.noreply.github.com>
2023-01-12 22:03:38 +0530

    Delete WhenTheIDE directory

--- WhenTheIDE/static/result.html/script_result.js
+++
@@ -1,4 +0,0 @@
-console.log("Tumhari mummy")
-console.log("Food dedo koi pls nahi to me ro dunga")
-console.log("HACKER HEART MUNDA INDIAN PROUD MUKESH PAN WALLA 69")
-console.log("If you are looking at this, these sentences are actually features and not a gimmick.")
